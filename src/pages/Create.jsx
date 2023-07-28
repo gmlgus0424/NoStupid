0ba04eb5f24e8c29de5d679685292d3c5adfbcdb
kso2050 <saeyeon9002@naver.com>
2023-07-28 22:55:16 +0900

    유효성 검사 훅 폼 사용해 다시 수정 중...

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react'
-import {useForm} from 'react-hook-form';
+import { useForm } from "react-hook-form";
 
 const User ={
     name: '홍길동',
@@ -12,15 +12,17 @@ const User ={
 
 export default function Create() {
 
-    const {register, watch, errors} = useForm();
-    console.log(watch('email'))
+    const {register, watch, formState:{errors}, handleSubmit, getValues} = useForm();
+    console.log(watch('rpw'))
     const password = useRef();
     password.current=watch("password");
+    
     const onSubmit = (data) => {
-        console.log('data', data)
+        console.log(data)
 
         //axios.post('/', data)
     }
+
 
     const [name, setName]= useState("");
     const [birth,setBirth]= useState("");
@@ -34,6 +36,33 @@ export default function Create() {
     const [rpwVaild, setRpwVaild] = useState(false);
     const [notAllow, setNotAllow] = useState(true);
 
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+        const regex =  /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+        if(regex.test(email)){
+            setEmailVaild(true);
+        }else{
+            setEmailVaild(false);
+        }
+    }
+    const handlePw = (e) => {
+        setPw(e.target.value);
+        const regex1 =  /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,20}$/;
+        if(regex1.test(pw)){
+            setPwVaild(true);
+        }else{
+            setPwVaild(false);
+        }
+    }
+    const handleRpw = (e) => {
+        setRpw(e.target.value);
+        const regex2 =  /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,20}$/;
+        if(regex2.test(rpw)){
+            setRpwVaild(true);
+        }else{
+            setRpwVaild(false);
+        }
+    }
     
     const onClickConfirmButton = () => {
         if(email == User.email){
@@ -44,107 +73,110 @@ export default function Create() {
     }
 
     useEffect(() => {
-        if (emailVaild && pwVaild) {
+        if (emailVaild && pwVaild) { // 버튼을 풀어 줌
             setNotAllow(false);
             return;
         }
-        setNotAllow(true);
+        setNotAllow(true); //버튼 비활성화
     },[emailVaild,rpwVaild]);
 
+
   return (
-    <div className="page">
-        <div className="container">
-            <div className="back">
-                <img className ="backImg" src="img/r2.png" alt="r2"></img>
+    <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="page">
+            <div className="container">
+                <div className="back">
+                    <img className ="backImg" src="img/r2.png" alt="r2"></img>
+                </div>
+
+
+                <div className="title">
+                        <p className='titleWrap'>회원가입</p>
+                </div>
+                
+                
+                <div className="content">
+                    <div className='nameInput'>
+                        <input className='name' 
+                        placeholder='이름'
+                        value={name} 
+                        onChange={(e)=>setName(e.target.value)}/>
+                    </div>
+
+                    <div className='birthInput'>
+                        <input className='birth' 
+                        placeholder='생년월일'
+                        value={birth} 
+                        onChange={(e)=>setBirth(e.target.value)}/>
+                    </div>
+
+                    <div className='numInput'>
+                        <input className='num' 
+                        placeholder='전화번호'
+                        value={num} 
+                        onChange={(e)=>setNum(e.target.value)}/>
+                    </div>
+
+                    <div className='mailInput'>
+                        <input 
+                        type='text'
+                        className='mail' 
+                        value={email}
+                        onChange={handleEmail}
+                        placeholder='E-mail'  
+                        />
+                    </div>
+
+                    <div className='errorMessage1'>
+                        { !emailVaild && email.length > 0 && 
+                            (<p>이메일 형식이 올바르지 않습니다.</p>)}
+                    </div>
+                        
+                    <div className='pwInput'>
+                        <input 
+                        type='password'
+                        className='pw'
+                        value={pw} 
+                        onChange={handlePw}
+                        placeholder='Password'/> 
+                    </div>
+
+                    <div className='errorMessage2'>
+                        {!pwVaild && 0 < pw.length && pw.length< 8 &&
+                            (<p>비밀번호는 8자리 이상 설정해야 합니다.</p>)}
+                    </div>
+
+                    <div className='rpwInput'>
+                        <input 
+                        type='password'
+                        className='rpw' 
+                        placeholder='reconfirm password' 
+                        {...register ('rpw',{
+                            required: true,
+                            validate:{ 
+                                check: val => {
+                                    if(getValues('pw') !== val){
+                                        return "비밀번호가 일치하지 않습니다."} 
+                                    }}})}
+                        />
+                    </div>
+
+                    <div className='errorMessage3'>
+                        {errors.rpw && <p>{errors.rpw.message} 비밀번호가 일치하지 않습니다.</p>}
+                    </div>
+                </div>
+                        
+                        
+                <div className='submit'>
+                    <button 
+                    onClick={onClickConfirmButton}
+                    disabled={notAllow} 
+                    className='nextButton'>
+                        <p className='next'>SIGN UP</p>
+                    </button>
+                </div>
             </div>
-
-
-            <div className="title">
-                    <p className='titleWrap'>회원가입</p>
-            </div>
-            
-            
-            <div className="content">
-                <div className='nameInput'>
-                    <input className='name' 
-                    placeholder='이름'
-                    value={name} 
-                    onChange={(e)=>setName(e.target.value)}/>
-                </div>
-
-                <div className='birthInput'>
-                    <input className='birth' 
-                    placeholder='생년월일'
-                    value={birth} 
-                    onChange={(e)=>setBirth(e.target.value)}/>
-                </div>
-
-                <div className='numInput'>
-                    <input className='num' 
-                    placeholder='전화번호'
-                    value={num} 
-                    onChange={(e)=>setNum(e.target.value)}/>
-                </div>
-
-                <div className='mailInput'>
-                    <input 
-                    type='email'
-                    className='mail' 
-                    {...register("email",{
-                        required: true, 
-                        pattern: /^\S+@\S+$/i  })}
-                    placeholder='E-mail' 
-                    value={email} />
-                </div>
-
-                <div className='errorMessage1'>
-                    {errors?.email && <p>이메일 형식이 올바르지 않습니다.</p>}
-                </div>
-                    
-                <div className='pwInput'>
-                    <input 
-                    type='password'
-                    name ='pw'
-                    className='pw'
-                    {...register('pw',{
-                        required: true,
-                        minLength: 8
-                    })} 
-                    placeholder='Password'
-                    value={pw} /> 
-                </div>
-
-                <div className='errorMessage2'>
-                    {errors?.pw && <p>비밀번호는 8자리 이상 설정해야 합니다.</p>}
-                </div>
-
-                <div className='rpwInput'>
-                    <input 
-                    type='password'
-                    name = 'rpw'
-                    className='rpw' 
-                    {...register ('rpw',
-                        {required: true}
-                        ,{validate: (value) => value === password.current})}
-                    placeholder='reconfirm password' 
-                    value={rpw}/>
-                </div>
-
-                <div className='errorMessage3'>
-                    {errors?.rpw && <p>비밀번호가 일치하지 않습니다.</p>}
-                </div>
-            </div>
-                    
-                    
-            <div className='submit'>
-                <button  
-                onClick={onClickConfirmButton}
-                disabled={notAllow} 
-                className='nextButton'>
-                    <p className='next'>SIGN UP</p>
-                </button>
-            </div>
-         </div>
-    </div>
+        </div>
+    </form>
   )
 }
