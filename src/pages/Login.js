356a4af037175ff1ee23fa9db89d0cb0486e04f3
gmlgus0424 <gmlgus0424@naver.com>
2023-07-28 20:43:27 +0900

    로그인 css 완성

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,39 +11,113 @@ display : flex;
 height: calc(var(--vh, 1vh) * 100);
 flex-directio : column;
 justify-content: space-between;
+
 input{
   width: 100%;
-  height: 44px;
-  outline : 1px solid #949494;
+  padding : 15px 2px; 
   border-radius : 10px;
-  
+  margin-bottom: 14px;
+ border-width :1px;
+ border-color: grey;
+
+input::placeholder{
+  color:#949494;
+  font-size : 16px;
 }
+input:focus {
+  border-color: black; 
+}`;
+
+
+const LogoBox = styled.div`
+`;
+
+const LoginArea = styled.div`
+width: 100%;
+`;
+const LoginWrapper = styled.div`
+display : block;
+width : 100%;
+outline-style:grey solid 1px ;
+
+`;
+const EmailInput= styled.input `
+display: block;
+width: 100%;
+height : 16px;
+
+&::placeholder {
+  color: ##949494;
+  font-size : 16px;
+}
+&:focus {
+  color: black;
+}
+`;
+
+const ErrorAlter = styled.span`
+display:inline-block;
+font-size: 12px;
+color :  #ff003e;
+letter-spacing: -.5px;
+vertical-align: middle;
+`;
+const PasswordWrapper= styled.div`
+display : block;
+width : 100%;
+`;
+
+const PasswordInput = styled.input`
+display: block;
+width: 100%;
+height : 16px;
+&::placeholder {
+  color: ##949494;
+  font-size : 16px;
+}
+&:focus {
+  color: black;
+}
+`;
+const LoginButton = styled.button`
+margin-top:25px;
+font-weight: 600;
+color: #ffffff;
+font-size: 14px;
+background-color:#ACABAB;
+border-radius : 10px;
+transition: all 300ms ease-in-out;
+padding: 15px 0;
+width: 100%;
+font-size: 14px;
+border:0 solid black;
+
+`;
+const KakaoLoginBtn= styled.a`
+background-color: #FEE500;
+width :100%;
+margin-top : 32px
+margin-bottom: 40px;
+cursor: pointer;
+border-radius: 5px;
 
 
 `;
 
 
 
-const LogoBox = styled.div``;
-const LoginArea = styled.div``;
-const LoginWrapper = styled.div``;
-const PasswordWrapper= styled.div``;
-const LoginButton = styled.div``;
-const KakaoLoginBtn= styled.a``;
-
-
 
 
 function Login(){
- const {register, handleSubmit, watch,formState:{errors}}=useForm();
+ const {register, handleSubmit ,formState:{errors}}=useForm();
   //결과값을 표시할 result로 가지고옴
 const [result,setResult]=useState('');
 
-const watchemail= watch('email','');
+
 
 const onSubmit =(data)=>{
     setResult(JSON.stringify(data));
- }
+}
 
   return (
     <Container>
@@ -52,20 +126,22 @@ const onSubmit =(data)=>{
     <LoginArea>
     <form onSubmit={handleSubmit(onSubmit)}>
       <LoginWrapper>
-     <input type="email"
+     <EmailInput 
+        type="email"
      {...register("email",{
       required:true,
       pattern : /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i,
      })} placeholder='E-mail'/>
      {
-      errors?.type==='required' && <p>반드시 이메일을 입력하여주세요</p>
+      errors.email?.type ==='required' && (<ErrorAlter>반드시 이메일을 입력하여주세요</ErrorAlter>)
      }
      {
-      errors?.type==='pattern' && <p>올바르게 이메일을 입력해주세요 </p>
+      errors.email?.type ==='pattern' && (<ErrorAlter>올바르게 입력해주세요</ErrorAlter>)
      }
 </LoginWrapper>
+
 <PasswordWrapper>
-     <input type ="password" 
+     <PasswordInput type ="password" 
      {...register("password",{
       required : true,
       minLength: {
@@ -74,21 +150,24 @@ const onSubmit =(data)=>{
       },
      })} placeholder="password"/> 
      {
-      errors?.type==='required' && <p>비밀번호를 입력하세요.</p>
+      errors.password?.type==='required' && (<ErrorAlter>비밀번호를 입력해주세요</ErrorAlter>)
      }
      {
-      errors?.type==='minLength' && <p>최소 6자 이상의 비밀번호를 입력해주세요</p>
-     }
+      errors.password?.type==='minLength' && (<ErrorAlter>최소 6자 이상 입력해주세요</ErrorAlter>)}
     </PasswordWrapper>
-     <LoginButton type="submit">로그인</LoginButton>
+     <LoginButton type="submit">LOGIN</LoginButton>
       
-          <p>{watchemail}</p>
-   
     </form>
-    <KakaoLoginBtn>카카오로그인</KakaoLoginBtn>
+    <KakaoLoginBtn href={``}>카카오로그인</KakaoLoginBtn>
+ 
+
+   
     </LoginArea>
     </LogoBox>
     </Container>
+
+
+  
   )
 };
 
